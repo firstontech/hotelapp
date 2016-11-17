d74e145300e2df9a02939656e1b1584e39883aa3
Malik Itopa <itopa@live.ca>
2016-11-16 21:57:19 -0800

    added doctypes

---
+++ hotel/hotel_management/doctype/hotel_reservation/hotel_reservation.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2016, FirstOnTech and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Hotel Reservation', {
+	refresh: function(frm) {
+
+	}
+});
---
+++ hotel/hotel_management/doctype/room_type/room_type.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2016, FirstOnTech and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Room Type', {
+	refresh: function(frm) {
+
+	}
+});
